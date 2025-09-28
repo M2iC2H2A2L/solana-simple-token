8e3d4fb7b7a5eb138bf0070fbd5f129e62deae12
MG-CYPTO <147055796+M2iC2H2A2L@users.noreply.github.com>
2025-09-28 13:03:53 +0200

    Add basic test for mint instruction.

---
+++ tests/lib.rs
@@ -0,0 +1,2 @@
+rust<br>use super::*;<br>#[cfg(test)]<br>mod tests {<br>    use super::*;<br>    #[test]<br>    fn test_create_mint() {<br>        assert_eq!(1, 1); // Placeholder test<br>    }<br>}<br>
+
